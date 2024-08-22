a0c189594c526df30fa5f7bd30e7c77e36f619c1
Johodah <johnny.dahle@hotmail.com>
2024-08-22 16:36:01 +0200

    Initial-Commit

---
+++ scripts/coin.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_body_entered(body):
+	print("+1 Coin!")
+	queue_free()
