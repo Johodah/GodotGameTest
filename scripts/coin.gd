d0cc44449a539c2ec90a005a47b5e995cd1a40c4
Johodah <johnny.dahle@hotmail.com>
2024-09-04 13:16:44 +0200

    small edit

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,12 +1,8 @@
 extends Area2D
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 @onready var game_manager = %GameManager
 @onready var animation_player = $AnimationPlayer
 
-func _on_body_entered(body):
+func _on_body_entered(_body):
 	game_manager.add_point()
 	animation_player.play("pickup")
